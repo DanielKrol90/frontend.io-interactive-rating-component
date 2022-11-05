37f8161b9857399b72a6b62bdeeb4d7388bb1c94
Daniel Król <danielkrol90@gmail.com>
2022-11-05 22:07:51 +0100

    fixing spell-errors

--- js/main.js
+++ js/main.js
@@ -1,37 +1,33 @@
-const scoreBtns = document.querySelectorAll('.button__secondary');
-const submitBtn = document.querySelector('.button__primary');
-const ratingText = document.querySelector('.thanks__rate-value-answer');
-const ratingCard = document.querySelector('.rating__card');
-const thanksCard = document.querySelector('.thanks__card');
+const btnRate = document.querySelectorAll('.button__secondary');
+const btnSubmit = document.querySelector('.button__primary');
+const textValue = document.querySelector('.thanks__rate-value-answer');
+const cardRating = document.querySelector('.rating__card');
+const cardThanks = document.querySelector('.thanks__card');
 const rateNotSelected = document.querySelector('.rating__not-selected');
 
-let rating;
+let value;
 
-//Loops through score buttons to see which one was clicked and stores the rating
+//function for loop for each elements, storing the rating and showing the rating on thanks card
 
-scoreBtns.forEach((score) => {
-    score.addEventListener('click', (e) => {
+btnRate.forEach((rating) => {
+    rating.addEventListener('click', (e) => {
         e.preventDefault();
-        rating = e.target.innerHTML;
-        score.style.backgroundColor = "hsl(25, 97%, 53%)";
-        score.style.color = "hsl(0, 0%, 100%)";
-        ratingText.innerHTML = `You selected ${rating} out of 5`;
+        value = e.target.innerHTML;
+        rating.style.backgroundColor = "hsl(25, 97%, 53%)";
+        rating.style.color = "hsl(0, 0%, 100%)";
+        textValue.innerHTML = `You selected ${value} out of 5`;
     })
 });
 
+// funcion for submit click, after submit hide rateing card and show thanks card. If user didnt choose a rating it block and send a message for chose a rating. After chosing rating send to thanks card
 
-//Listens for submit button click, then hides rating card and shows thank you card
-//Also checks to see if the user submitted a rating.  If not, provides a message.
-// If user not submitted a rating, blocks card until rate is selected
-
-
-submitBtn.addEventListener('click', (e) => {
+btnSubmit.addEventListener('click', (e) => {
     e.preventDefault();
-    if (!rating) {
+    if (!value) {
         rateNotSelected.innerHTML = "Choose a rating befoce submiting...";
     }
-    if (rating) {
-        ratingCard.style.display = "none";
-        thanksCard.style.display = "flex";
+    if (value) {
+        cardRating.style.display = "none";
+        cardThanks.style.display = "flex";
     }
 });
