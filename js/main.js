6267a8b206196907a71de9a4e0fa2afe44daa2ba
Daniel Król <danielkrol90@gmail.com>
2022-11-05 20:39:46 +0100

    preparing variable to work on them

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const scoreBtns = document.querySelectorAll('.button__secondary');
+const submitBtn = document.querySelector('.button__primary');
+const ratingText = document.querySelector('.thanks__rate-value-answer');
+const ratingCard = document.querySelector('.rating__card');
+const thanksCard = document.querySelector('.thanks__card');
